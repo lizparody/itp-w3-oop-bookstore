f48abc5069b9c960f40a8788f881d4006ae1f269
Liz Parody <lizparody@gmailo.com>
2017-08-15 17:08:15 +0000

    Finished the oop bookstore exercise

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,44 @@
 class Bookstore(object):
-    pass
-
-
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+   
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def get_books(self):
+        return self.books
+        
+    def search_books(self, title = None, author = None):
+      result = []
+      if (title == None) and author:
+        for book in self.books:
+          if author.name == book.author.name:
+            result.append(book)
+      elif title and (author == None):
+        for book in self.books:
+          if title.lower() in book.title.lower():
+            result.append(book)
+      return result
+      
+        
 class Author(object):
-    pass
-
-
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def get_books(self):
+        return self.books
+        
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.author.add_book(self)
+        
+
+
